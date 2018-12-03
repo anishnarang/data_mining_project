a3f0f9f94f17f2f3fc1c8b44f63611a510b7c2c2
arjun-19 <93.arjun@gmail.com>
2018-12-02 20:07:57 -0800

    final part

---
+++ final_step.py
@@ -0,0 +1,31 @@
+import math
+import time
+import sys
+from operator import itemgetter
+from collections import defaultdict
+import pickle
+
+class FindBusiness:
+
+	def __init__(self, business_to_food, food_to_business):
+		self.business_to_food = business_to_food
+		self.food_to_business = food_to_business
+
+	def suggestBusiness(self, user1, user2, foods):
+		final_business = defaultdict()
+		for food in foods:
+			candidate_business = defaultdict()
+			businesses = self.food_to_business[food]
+			for business in businesses:
+				rating = self.business_to_food[business][food]
+				candidate_business[business] = rating
+			final_business[food] = sorted(candidate_business, key = candidate_business.get, reverse=True)[:5]
+		return final_business
+
+if __name__ == '__main__':
+
+	foods = ["cheese", "bread", "chicken", "potato", "chocolate"]
+	business_to_food = pickle.load(open( "business_to_food_mapping.pkl", "rb" ))
+	food_to_business = pickle.load(open( "food_to_business_mapping.pkl", "rb" ))
+	findBusiness = FindBusiness(business_to_food, food_to_business)
+	businesses = findBusiness.suggestBusiness('8HMN1jABE5y7my-B8m62Cw', '-X6NbB699wt1Z2atOPgmBw', foods)
